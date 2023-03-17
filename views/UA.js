d0db471f875d69f28e3f6b1487b334ee56cb6c09
Seen3 <littlefinger331@gmail.com>
2023-03-17 21:08:34 +0530

    Tried Login API

--- views/UA.js
+++ views/UA.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+
 const SignUp =({ navigation })=>{
     const [text, onChangeText] = React.useState('');
     const [text2, onChangeText2] = React.useState('');
     const goToPI = () => {
         navigation.navigate('PI');
       };
+    async function handleSignUp(event) {
+        event.preventDefault();
+        const user=text;
+        const pass=text2;
+        axios.post(`http://localhost:6699/SignUp`,{user,pass}).then(res=>{
+            console.log(res);
+        })
+        await goToPI();
+    }
     return(
         <View>
             <Text>Please Enter Details and Create a Password</Text>
@@ -20,7 +32,7 @@ const SignUp =({ navigation })=>{
             secureTextEntry={true}
             value={text2}
             placeholder={'Password'} />
-            <Button title='Sign Up' onPress={goToPI}></Button>
+            <Button title='Sign Up' onPress={handleSignUp}></Button>
         </View>
     )
     
