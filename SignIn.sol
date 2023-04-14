c1a2760da4ff2df4a1fd64ae6b8615b15344da69
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2023-04-14 11:17:39 +0530

    Update SignIn.sol

--- SignIn.sol
+++ SignIn.sol
@@ -2,11 +2,15 @@ pragma solidity ^0.8.18;
 //SPDX-License-Identifier:MIT
 
 contract SignUpIn{
-    mapping(string => string) private userPasswords;
-    function setPassword(string memory username, string  memory passwordHash) public{
+    mapping(string => string) public userPasswords;
+    function setPassword(string memory username, string memory passwordHash) public{
         userPasswords[username] = passwordHash;
     }
-    function getPassword(string memory username) public view returns (string memory) {
-        return userPasswords[username];
+    event PasswordReturned(string password);
+
+    function getPassword(string memory username) public returns (string memory) {
+        string memory password = userPasswords[username];
+        emit PasswordReturned(password);
+        return password;
     }
 }
