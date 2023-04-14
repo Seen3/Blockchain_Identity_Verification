53f73e4fc9f45eff57a7cd1aa6cf6e19f59b2938
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2023-04-14 11:17:58 +0530

    Update Details.sol

--- Details.sol
+++ Details.sol
@@ -1,18 +1,32 @@
 pragma solidity ^0.8.18;
 //SPDX-License-Identifier:MIT
 
-contract MakeDetails{
-    struct Person{
-    string name;
-    string dob;
-    string add;
-    string aadhar;
-}
-    mapping(string => Person) private People;
-    function setDetails(string memory name, string memory dob, string memory add,string memory aadhar) public {
-        People[aadhar] = Person(name,dob,add,aadhar);
+contract MakeDetails {
+    struct Person {
+        string name;
+        string dob;
+        string add;
+        string aadhar;
     }
-    function getDetails(string memory aadhar) public view returns (Person memory) {
-        return People[aadhar];
+    
+    mapping(string => Person) private People;
+    
+    function setDetails(string memory name, string memory dob, string memory add, string memory aadhar) public {
+        People[aadhar] = Person(name, dob, add, aadhar);
+    }
+    
+    event PersonReturned(string json);
+    
+    function getDetails(string memory aadhar) public {
+        Person memory man = People[aadhar];
+        
+        string memory json = string(abi.encodePacked(
+            '{"name":"', man.name, '",',
+            '"dob":"', man.dob, '",',
+            '"add":"', man.add, '",',
+            '"aadhar":"', man.aadhar, '"}'
+        ));
+        
+        emit PersonReturned(json);
     }
 }
